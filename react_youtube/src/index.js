d0cbc27d6fe8a34bcf3c70d876fd21873d7969cb
khoerburger <59145854+khoerburger@users.noreply.github.com>
2022-05-08 20:24:13 -0500

    Add files via upload

---
+++ react_youtube/src/index.js
@@ -0,0 +1,12 @@
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+ReactDOM.render(
+//   <div>
+    <h1>Hello World!</h1>
+    /* <p>This is a paragraph.</p>
+    console.log("This is a log")
+</div>, */,
+  document.getElementById("root")
+);
